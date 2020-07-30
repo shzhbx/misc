0062c2764ffa79baea0822e328bea9c68921fbe5
shawn <shzhbx@gmail.com>
2020-07-31 03:45:34 +0800

    update file and dir

--- UNIX_APUE/4.file_and_dir/filedir_filecheck.c
+++ UNIX_APUE/4.file_and_dir/filedir_filecheck.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 #define CHECK_FILE "/dev/fd/1"
 
 static void check(const char * filename)
 {
     struct stat st;
-    if (stat(filename, &st) == 0)
+    //if (stat(filename, &st) == 0)
+    if (fstatat(AT_FDCWD, filename, &st, AT_SYMLINK_NOFOLLOW) == 0)
     {
         printf("st.st_rdev      [%d]        \n", st.st_rdev);
         printf("st.st_dev       [%d]        \n", st.st_dev);
@@ -20,6 +22,40 @@ static void check(const char * filename)
         printf("st.st_atimespec [%ld,%ld]   \n", st.st_atimespec.tv_nsec, st.st_atimespec.tv_sec);
         printf("st.st_ctimespec [%ld,%ld]   \n", st.st_ctimespec.tv_nsec, st.st_ctimespec.tv_sec);
         printf("st.st_mtimespec [%ld,%ld]   \n", st.st_mtimespec.tv_nsec, st.st_mtimespec.tv_sec);
+
+        mode_t mode = st.st_mode;
+        if (S_ISREG(mode))
+        {
+            printf("[%s] is regular file \n", filename);
+        }
+        else if (S_ISLNK(mode)) 
+        {
+            printf("[%s] is link file\n", filename);
+        }
+        else if (S_ISCHR(mode)) 
+        {
+            printf("[%s] is char special file\n", filename);
+        }
+        else if (S_ISBLK(mode)) 
+        {
+            printf("[%s] is block special file\n", filename);
+        }
+        else if (S_ISFIFO(mode)) 
+        {
+            printf("[%s] is fifo file\n", filename);
+        }
+        else if (S_ISSOCK(mode)) 
+        {
+            printf("[%s] is socket file\n", filename);
+        }
+        else if (S_ISDIR(mode)) 
+        {
+            printf("[%s] is dir file\n", filename);
+        }
+    }
+    else 
+    {
+        perror("stat ");
     }
 }
 
